4b6e7ff43f6220d538f1ba470a4b15dfb242b46b
Amazingandyyy <amazingandyyy@gmail.com>
2017-05-29 12:15:20 -0700

    update

--- 06_constants/04_iota/main.go
+++ 06_constants/04_iota/main.go
@@ -5,11 +5,11 @@ import "fmt"
 const (
 	a = iota // 0
 	b        // 1
-	// c        // 2
+	c        // 2
 )
 
 func main() {
 	fmt.Println(a)
 	fmt.Println(b)
-	// fmt.Println(c)
+	fmt.Println(c)
 }
